6e91f3fb2b245b9e8d3d4f2c96c72937ade23f13
Mateusz Ożóg <mateusz2345678@gmail.com>
2020-06-04 14:55:54 +0200

    :tada: Contact form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="container mb-5 pb-5">
 		<div class="row">
-			<div class="col-lg-5 ml-auto">
+			<div class="col-lg-7 ml-auto">
 				<div class="card">
 					<div class="card-header">
 						<h1 class="mb-0">Kontakt</h1>
@@ -9,7 +9,25 @@
 					<div class="card-body">
 						<div class="form-group">
 							<label for="name">Twoje imię</label>
-							<input type="text" name="name" id="name" class="form-control" />
+							<input type="text" name="name" id="name" class="form-control" placeholder="Jan" />
+						</div>
+						<div class="form-group">
+							<label for="surname">Twoje nazwisko</label>
+							<input type="text" name="surname" id="surname" class="form-control" placeholder="Kowalski" />
+						</div>
+						<div class="form-group">
+							<label for="nickname">Twoje przezwisko</label>
+							<input type="text" name="nickname" id="nickname" class="form-control" />
+						</div>
+						<div class="form-group">
+							<label for="message">Twoja wiadomość</label>
+							<textarea name="message" id="message" rows="5" class="form-control" placeholder="Aa..."></textarea>
+						</div>
+						<div class="text-right">
+							<button class="btn btn-success">
+								Wyślij wiadomość
+								<i class="bx bx-envelope"></i>
+							</button>
 						</div>
 					</div>
 				</div>
